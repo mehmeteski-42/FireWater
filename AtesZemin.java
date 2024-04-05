025a6175f2f7af787fa6c4a35931671c78d711c7
tuzgen <oguztuzgen@gmail.com>
2024-04-05 22:43:19 +0300

    anan

--- AtesZemin.java
+++ AtesZemin.java
@@ -3,5 +3,6 @@ import java.awt.*;
 public class AtesZemin extends Zemin {
     public AtesZemin(int x, int y, int sizex, int sizey){
         super(x,y,sizex,sizey,false,Color.RED);
+        System.out.println("zarttiri zort zort");
     }
 }
